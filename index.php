a1173d88072f5a6d77a3ba57a2db427468299c3a
dhanukprabin <dhanukprabin111@gmail.com>
2025-01-23 11:34:37 +0545

    changes in index

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-echo '<h1>Welcome to the world of DevOps</h1>';
+echo '<h1>Welcome to the world of DevOps</h1>
+	<h2>Welcome to the world of DevOps</h2>
+	<h3>Welcome to the world of DevOps</h3>';
 ?>
 
